c8037524f5c88f16fb676576e2438643cdea56b3
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2020-03-23 11:23:12 +0100

    Test
    test

--- wordplate/public/themes/gathenhielmska/index.php
+++ wordplate/public/themes/gathenhielmska/index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()): ?>
+<?php if (have_posts()) : ?>
 
-    <?php while (have_posts()): the_post(); ?>
+    <?php while (have_posts()) : the_post(); ?>
 
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
