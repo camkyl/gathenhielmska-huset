f92f7bf0f0a3b67812fcf98fca05fb0000e3912f
Camilla <cks@live.se>
2020-04-02 10:55:28 +0200

    preparing for acf

---
+++ wordplate/public/themes/gathenhielmska/fields/changemyname.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+/*
+if (function_exists('acf_add_local_field_group')) {
+
+    acf_add_local_field_group(array(
+        'key' => 'group_5e5e578f755b3',
+        'title' => 'Student info',
+        'fields' => array(
+            array(
+                'key' => 'field_5e5e579c604a4',
+                'label' => 'Email',
+                'name' => 'email',
+                'type' => 'email',
+                'instructions' => 'Add student email',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+            ),
+            array(
+                'key' => 'field_5e5e57b4604a5',
+                'label' => 'Github profile',
+                'name' => 'github_profile',
+                'type' => 'url',
+                'instructions' => 'Students Github profile',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'student',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'side',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+}
+*/
+
+
+// endif;
---
+++ wordplate/public/themes/gathenhielmska/single-changemyname.php
@@ -0,0 +1,45 @@
+<?php get_header(); ?>
+
+<!--In this file we style the page. Content is added in the sidebar in Program/students (we need to add ACF to plugins)-->
+
+<section>
+    <?php if (have_posts()) : ?>
+
+        <?php while (have_posts()) : the_post(); ?>
+
+            <!--This will be the title of the student, in our case the program or a person working at gathenhielmska-->
+            <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+            <h2>Contact:</h2>
+            <a href="mailto:<?php //the_field('email');
+                            ?>"><?php // the_field('email');
+                                ?></a>
+
+            <a href="<?php // the_field('github_profile');
+                        ?>">Go to Github profile --></a>
+
+            <div class="student-image">
+                <?php //if (get_field('image')) :
+                ?>
+                <?php // $image = get_field('image');
+                ?>
+                <img src="<?php // echo $image['url']
+                            ?>" alt="something">
+                <?php // else:
+                ?>
+                <img src="" alt="placeholder">
+                <?php // endif;
+                ?>
+            </div>
+
+            <p><?php the_content(); ?></p>
+
+            <small><?php the_date(); ?></small>
+
+        <?php endwhile; ?>
+
+    <?php endif; ?>
+</section>
+
+
+<?php get_footer(); ?>
