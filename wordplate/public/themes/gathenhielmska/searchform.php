3f974fe5fed9e960b97d1d920a1309be6125220a
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2020-04-09 16:22:43 +0200

    Footer started w& CSS

--- wordplate/public/themes/gathenhielmska/searchform.php
+++ wordplate/public/themes/gathenhielmska/searchform.php
@@ -13,5 +13,5 @@
     <label for="search">Search in <?php echo home_url('/'); ?></label>
     <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
     <!--Fix path of search icon!!-->
-    <input type="image" alt="Search" src="<?php bloginfo('template_url'); ?>/resources/icons/search-icon.svg" />
+    <input type="image" alt="Search" src="<?php bloginfo('template_url'); ?>/public/icons/search-icon.svg" />
 </form>
