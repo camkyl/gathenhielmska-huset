59225d14bc68255d0235cd7a7768c5d364c920c9
Camilla <cks@live.se>
2020-04-09 10:38:54 +0200

    small fixes

--- wordplate/public/themes/gathenhielmska/template-parts/blocks/businesses/businesses.php
+++ wordplate/public/themes/gathenhielmska/template-parts/blocks/businesses/businesses.php
@@ -7,10 +7,12 @@
 
 $name = get_field('name');
 $role = get_field('role');
+$website = get_field('website');
 
 ?>
 
 <article class="businesses__article">
     <p class="--title"><?php echo $name; ?></p>
     <p class="--role"><?php echo $role; ?></p>
+    <a class="--website"><?php echo $website; ?></a>
 </article>
