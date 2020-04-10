abbde73368921b54af8a3af7f3a430b232b45ca2
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2020-04-10 15:29:05 +0200

    Updates: - Implemented search function on menu

--- wordplate/resources/scripts/app.js
+++ wordplate/resources/scripts/app.js
@@ -1,12 +1,17 @@
 const burger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
+const search = document.querySelector(".search");
+const searchIcon = document.querySelector(".search-icon");
 
 const navSlide = () => {
   burger.addEventListener("click", () => {
 
     burger.classList.toggle("toggle");
     nav.classList.toggle("nav-active");
+  })
 
+  searchIcon.addEventListener("click", () => {
+    search.classList.toggle("search-active");
   })
 }
 
