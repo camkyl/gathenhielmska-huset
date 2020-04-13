567e84e3b730098d53f81d12719422606b60927e
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2020-04-13 21:09:46 +0200

    Update: - Created contact page - Added form plugin - Styled page

--- wordplate/public/themes/gathenhielmska/page-templates/contact.php
+++ wordplate/public/themes/gathenhielmska/page-templates/contact.php
@@ -3,6 +3,15 @@
 <?php get_header(); ?>
 
 <!--The content in the admin page-->
+
+<div class="page-title">
+    <div class="heading-wrapper">
+        <div class="heading-wrapper__left"></div>
+        <h3>Kontakt</h3>
+        <div class="heading-wrapper__right"></div>
+    </div>
+</div>
+
 <?php if (have_posts()) : ?>
 
     <?php while (have_posts()) : the_post(); ?>
@@ -14,5 +23,49 @@
     <?php endwhile; ?>
 
 <?php endif; ?>
+
+<div class="page-title">
+    <div class="heading-wrapper">
+        <div class="heading-wrapper__left"></div>
+        <h3>Ansvariga</h3>
+        <div class="heading-wrapper__right"></div>
+    </div>
+</div>
+
+<div class="in-charge-wrapper">
+    <div class="in-charge-wrapper__team">
+        <div class="in-charge-wrapper__team__left">
+            <h3>Joel Heirås</h3>
+            <p>Producent och samordnare</p>
+            <p>joe.heiras@gmail.com</p>
+            <p>0760-983832</p>
+        </div>
+        <div class="in-charge-wrapper__team__right">
+            <img src="../../../uploads/office.jpeg" alt="" class="team__image">
+        </div>
+    </div>
+    <div class="in-charge-wrapper__team">
+        <div class="in-charge-wrapper__team__left">
+            <h3>David Sperling Bolander</h3>
+            <p>Projektledare</p>
+            <p>sperlingbolander@gmail.com</p>
+            <p>0707-305134</p>
+        </div>
+        <div class="in-charge-wrapper__team__right">
+            <img src="../../../uploads/theoffice.png" alt="" class="team__image">
+        </div>
+    </div>
+    <div class="in-charge-wrapper__team">
+        <div class="in-charge-wrapper__team__left">
+            <h3>Isabel Lagos</h3>
+            <p>Projektledare och konstnärlig ledare</p>
+            <p>isabel@historieverket.se</p>
+            <p>0769-211107</p>
+        </div>
+        <div class="in-charge-wrapper__team__right">
+            <img src="../../../uploads/theoffice1.jpg" alt="" class="team__image">
+        </div>
+    </div>
+</div>
 
 <?php get_footer(); ?>
