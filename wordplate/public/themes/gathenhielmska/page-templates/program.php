b727a40183591326e945fe378887844522c3940c
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2020-04-16 12:34:25 +0200

    Program styling

--- wordplate/public/themes/gathenhielmska/page-templates/program.php
+++ wordplate/public/themes/gathenhielmska/page-templates/program.php
@@ -6,17 +6,21 @@
 
 <?php $posts = get_posts($args); ?>
 
-<div class="page-title">
+<div class="page-title program-title">
     <div class="heading-wrapper">
         <div class="heading-wrapper__left"></div>
         <h3>Program</h3>
         <div class="heading-wrapper__right"></div>
     </div>
 </div>
+
 <section class="events">
 
     <?php foreach ($posts as $post) : setup_postdata($post) ?>
-        <?php the_content(); ?>
+        <div class="">
+            <?php the_content(); ?>
+
+        </div>
     <?php endforeach; ?>
 
 </section>
