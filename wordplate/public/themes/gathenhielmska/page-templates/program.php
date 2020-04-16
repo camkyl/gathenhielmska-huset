28b031822fb6c0a3b658903a9281957fc38015ee
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2020-04-16 12:09:18 +0200

    Styling on program

--- wordplate/public/themes/gathenhielmska/page-templates/program.php
+++ wordplate/public/themes/gathenhielmska/page-templates/program.php
@@ -6,20 +6,18 @@
 
 <?php $posts = get_posts($args); ?>
 
-<section class="events">
-    <div class="page-title">
-        <div class="heading-wrapper">
-            <div class="heading-wrapper__left"></div>
-            <h3>Program</h3>
-            <div class="heading-wrapper__right"></div>
-        </div>
+<div class="page-title">
+    <div class="heading-wrapper">
+        <div class="heading-wrapper__left"></div>
+        <h3>Program</h3>
+        <div class="heading-wrapper__right"></div>
     </div>
+</div>
+<section class="events">
 
     <?php foreach ($posts as $post) : setup_postdata($post) ?>
-
         <?php the_content(); ?>
-        <a href="<?php the_permalink(); ?>" class="button">Läs mer</a>
-
     <?php endforeach; ?>
+
 </section>
 <?php get_footer(); ?>
