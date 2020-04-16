489671f76e7a8315dafb744bcd4d57337a819390
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2020-04-16 18:33:56 +0200

    Contact page done

--- wordplate/public/themes/gathenhielmska/page-templates/contact.php
+++ wordplate/public/themes/gathenhielmska/page-templates/contact.php
@@ -31,52 +31,64 @@
         <h3 class="contact-rooms__single__title">
             Salongen
         </h3>
-        <p class="contact-rooms__single__text"></p>
+        <p class="contact-rooms__single__text">
+            I salongen passar det att ha repetitioner, workshops och föreställningar och middagar. Max 30 personer får plats här inne.
+        </p>
     </div>
     <div class="contact-rooms__single">
         <img class="contact-rooms__single__img" src="../../uploads/kungsalen.png" alt="Italia room">
-        <h3 class="contact-rooms__single__title"></h3>
-        <p class="contact-rooms__single__text"></p>
+        <h3 class="contact-rooms__single__title">
+            Kungsalen
+        </h3>
+        <p class="contact-rooms__single__text">
+            Lämpar sig för möten, konferens, sammankomster och middagar för mindre sällskap. Max 10 personer.
+        </p>
     </div>
     <div class="contact-rooms__single">
         <img class="contact-rooms__single__img" src="../../uploads/galleriet.png" alt="Italia room">
-        <h3 class="contact-rooms__single__title"></h3>
-        <p class="contact-rooms__single__text"></p>
+        <h3 class="contact-rooms__single__title">
+            Galleriet
+        </h3>
+        <p class="contact-rooms__single__text">
+            Galleriutrymmena lämpar sig väl för galleriverksamhet, konserter och föreställningar.
+        </p>
     </div>
     <div class="contact-rooms__single">
         <img class="contact-rooms__single__img" src="../../uploads/versailles.png" alt="Italia room">
-        <h3 class="contact-rooms__single__title"></h3>
-        <p class="contact-rooms__single__text"></p>
+        <h3 class="contact-rooms__single__title">
+            Versailles
+        </h3>
+        <p class="contact-rooms__single__text">
+            Italiarummet passar bra för möten och mindre samlingar. Max 10 personer får plats här inne.
+        </p>
     </div>
     <div class="contact-rooms__single">
         <img class="contact-rooms__single__img" src="../../uploads/hyrindig.png" alt="Italia room">
-        <h3 class="contact-rooms__single__title"></h3>
-        <p class="contact-rooms__single__text"></p>
+        <h3 class="contact-rooms__single__title">
+            Hyr in dig
+        </h3>
+        <p class="contact-rooms__single__text">
+            Möjlighet finns generellt att tillfälligt hyra in sig för att arbeta i huset, under en dag, lite då och då eller under kortare perioder.
+        </p>
     </div>
-
-
 </div>
 
 <div class="contact-form-wrapper">
     <?php if (have_posts()) : ?>
-
         <?php while (have_posts()) : the_post(); ?>
 
             <h2 class="contact-title"><?php the_title(); ?></h2>
-
             <p class="contact-form-text">Om du vill boka våra rum eller komma i kontakt med oss av annan anledning, skriv gärna ett meddelande i formuläret nedan.</p>
-
             <?php the_content(); ?>
 
         <?php endwhile; ?>
-
     <?php endif; ?>
 </div>
 
 <div class="page-title contact-page">
     <div class="heading-wrapper">
         <div class="heading-wrapper__left"></div>
-        <h3>Ansvariga</h3>
+        <h3>Vi jobbar här</h3>
         <div class="heading-wrapper__right"></div>
     </div>
 </div>
@@ -91,7 +103,6 @@
             <p>0760-983832</p>
         </div>
 
-
     </div>
     <div class="in-charge-wrapper__team">
         <div class="in-charge-wrapper__team__left">
@@ -102,7 +113,6 @@
             <p>0707-305134</p>
         </div>
 
-
     </div>
     <div class="in-charge-wrapper__team">
         <div class="in-charge-wrapper__team__left">
@@ -112,7 +122,6 @@
             <p>isabel@historieverket.se</p>
             <p>0769-211107</p>
         </div>
-
 
     </div>
 </div>
